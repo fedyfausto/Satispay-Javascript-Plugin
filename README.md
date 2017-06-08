b5083bcf20c3fd929ad73614fcfdd9e0ba7deeef
Federico Santoro <federico.santoro@blackswangames.it>
2017-06-08 09:55:56 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Satispay-Javascript-Plugin
+A Javascript plugin to generate a dynamic payment request to Satispay service
+
+---
+
+##### Satispay is a new payment system that allows you to pay stores or friends from your smartphone. 
+
+Satispay allows you to perform microtransmissions completely free of charge (under € 10 Euros).
+
+The service allows you to add a WebButton, so you can manage the payment from your application or website but the WebButton is not dynamic so you can generate this one only refreshing your webpage and only clicking on the default Satispay Button.
+
+This plugin allows you to generate and open a dynamic window to get the payment from your users (and you can manage this with your classic events!)
+
+Install
+---
+
+Download and import the library with SCRIPT tag:
+
+```html
+<script src="/path_to_your_assets/satispay.js"></script>
+
+```
+or minified version:
+```html
+<script src="/path_to_your_assets/satispay.min.js"></script>
+
+```
+
+How to use
+---
+
+Before you can use the plugin, you need to initialize a Satispay object
+```Javascript
+var satispay = new Satispay(options);
+```
+Where the options are:
+```Javascript
+{
+    key : <String> Your merchant key,
+    webhook: <String> Your server URL hook,
+    sandbox: <Bool> Define if you are in Sandbox mode,
+    [key_sandbox: <String> Your sandbox merchant key]
+}
+```
+After initialization of the object you can call the main method to launch the  payment window:
+
+```Javascript
+satispay.newPayment(amount:<Int>,description:<String>,nameOfCallBackFunction:<String>);
+```
+* amount : The amount of your payment (e.g. use 200 for 2.00 €/$) 
+* description : The description for this payment, it will show into the window under the logo
+* nameOfCallBackFunction : The name of your callback function, this will be called after the payment verification
+
+Complete Example
+---
+```Javascript
+//Define the options
+var options = {
+    key : "YOUR_KEY",
+    webhook: "http://yourserver.yourdomain/webhook",
+    sandbox: false
+}
+
+//Initialize the Satispay object
+var satispay = new Satispay(options);
+
+//define a callback function
+var cb = function(){
+    console.log("The coffee has been Paid!");
+}
+
+//Pay one coffee
+satispay.newPayment(100,"Coffee Test","cb");
+
+//OR with a click event (e.g. Jquery):
+
+$("yourbutton_selector").on("click",function(){
+    satispay.newPayment(100,"Coffee Test","cb");
+});
+
+```
+Preview
+---
+In Desktop view:
+
+![alt text](http://i67.tinypic.com/2vjr6eh.png "Desktop view")
+
+In Mobile view:
+
+![alt text](http://i64.tinypic.com/2nh28tl.png "Mobile view")
+
+
+Be aware of Sandbox mode
+---
+If you want to test the sandbox you will need to accept the self-signed certificate of Satispay staging server, to do this click on this link and accept the certificate:
+https://staging.online.satispay.com/
---
+++ satispay.js
@@ -0,0 +1,83 @@
+/*!
+ * Satispay JavaScript Library v1.0.0
+ * https://github.com/fedyfausto/Satispay-Javascript
+ *
+ * Satispay Service
+ * https://www.satispay.com/
+ *
+ * Copyright Federico Fausto Santoro
+ * Released under the Apache 2.0 license
+ *
+ * Date: 2017-06-07
+ */
+
+(function() {
+    /**
+     * Constructor
+     */
+    this.Satispay = function() {
+        this.container = document.createElement("div");
+        this.container.style.display = "none";
+        document.body.appendChild(this.container);
+
+        var defaults = {
+            key: null,
+            webhook: null,
+            script_url:"https://online.satispay.com/checkout.min.js",
+            sandbox:false
+        }
+
+        if (arguments[0] && typeof arguments[0] === "object") {
+            this.options = extendDefaults(defaults, arguments[0]);
+            if(this.options.sandbox){
+                this.options.script_url="https://staging.online.satispay.com/checkout.min.js";
+            }
+        }
+    }
+
+    /**
+     * @method newPayment - Prepare and show the lightbox view to pay
+     * @param {Number} The amount of your payment, insert 200 for 2.00
+     * @param {String} The description for this payment, it will show into the window above the logo
+     * @param {String} The name of your callback function, this will called after the payment verification
+     */
+    Satispay.prototype.newPayment = function(amount,description,callback) {
+        // open code goes here
+        var $this = this;
+        $this.container.innerHTML = "";
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.onreadystatechange = function() { 
+            if (xmlHttp.readyState == 4 && xmlHttp.status == 200){
+                console.log("loaded");
+                $this.dom_script = document.createElement("script");
+                $this.dom_script.className = "satispay-button";
+                $this.dom_script.setAttribute('src', $this.options.script_url);
+                $this.dom_script.setAttribute('data-key', $this.options.sandbox ? $this.options.key_sandbox : $this.options.key );
+                $this.dom_script.setAttribute('data-amount', amount);
+                $this.dom_script.setAttribute('data-description', description);
+                $this.dom_script.setAttribute('data-webhook', $this.options.webhook);
+                $this.dom_script.setAttribute('data-callback', callback);
+                $this.container.appendChild($this.dom_script);
+                setTimeout(function(){
+                    var btn = document.getElementById('satisbutton');
+                    btn.click();
+                },1000);
+            }
+        }
+        xmlHttp.open("GET", $this.options.script_url, true); // true for asynchronous 
+        xmlHttp.send(null);
+    }
+
+    //private
+    function extendDefaults(source, properties) {
+        var property;
+        for (property in properties) {
+            if (properties.hasOwnProperty(property)) {
+                source[property] = properties[property];
+            }
+        }
+        return source;
+    }
+
+}());
